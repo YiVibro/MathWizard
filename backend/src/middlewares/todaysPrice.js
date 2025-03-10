b0e2768b0293b0e2e8b63f076204da3299a27156
YiVibro <149123685+YiVibro@users.noreply.github.com>
2025-03-10 20:48:50 +0530

    Delete backend/src directory

--- backend/src/middlewares/todaysPrice.js
+++
@@ -1,5 +0,0 @@
-const data=require("../data/price.json")
-
-exports.todaysPrice=(req,res)=>{
-    res.send("hello");
-}
--- backend/src/routes/itemRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllItems, addItem } = require('../controllers/itemController');
-
-const router = express.Router();
-
-router.get('/price', getAllItems);  // Get all items with prices
-router.post('/price', addItem); // Add new item (optional)
-
-module.exports = router;
